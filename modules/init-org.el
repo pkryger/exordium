4e2f2fb6672becedff0695653f3ab3a5085d1ac3
Przemek Kryger <pkryger@bloomberg.net>
2020-11-05 11:23:02 +0000

    Use C-c ; for iedit when in org-mode

--- modules/init-org.el
+++ modules/init-org.el
@@ -12,6 +12,9 @@
 (use-package org
   :commands (org-mode)
   :mode (("\\.org\\'" . org-mode))
+  :bind
+  (:map org-mode-map
+        ([remap org-toggle-comment] . #'iedit-mode))
   :custom
   (org-todo-keywords
    '((sequence "TODO(t)" "WORK(w!/!)" "WAIT(a@/!)" "|" "DONE(d!/!)")))
@@ -56,7 +59,6 @@
        (emacs-lisp . t)
        (C          . t)
        (dot        . t)))))
-
 
 
 ;;; Show org-mode bullets as UTF-8 characters.
