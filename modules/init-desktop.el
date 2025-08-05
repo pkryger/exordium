4f0c70f8805c3bc84e78a3d3b33e4740183af73b
Przemysław Kryger <pkryger@gmail.com>
2025-08-05 12:30:18 +0100

    feat(desktop): Refactor desktop configuration
    This tune up:
    * removes saving of buffers visiting Emacs and ELPA libraries,
    * avoids saving some ephemeral buffers,
    * limits number of buffers restored immediately.

--- modules/init-desktop.el
+++ modules/init-desktop.el
@@ -7,21 +7,56 @@
 
 (require 'saveplace)
 (require 'savehist)
-(require 'desktop)
 
-(add-to-list 'desktop-minor-mode-table '(company-posframe-mode nil))
+(use-package desktop
+  :ensure nil
+  :functions (exordium--restore-desktop)
+  :init
+  (defun exordium--restore-desktop ()
+    "Restore desktop."
+    (unless desktop-save-mode
+      (setq desktop-path (list user-emacs-directory))
+      (setq desktop-save t)
+      (message (format "Loading desktop from %s" desktop-path))
+      (desktop-read)
+      (desktop-save-mode 1)))
+  :custom
+  (desktop-files-not-to-save
+   (rx-to-string `(or
+                   ;; original value of `desktop-files-not-to-save'
+                   (seq string-start "/" (zero-or-more (not (any "/" ":"))) ":")
+                   (seq "(ftp)" string-end)
+                   ;; skip also Emacs and ELPA
+                   (seq string-start
+                        (or ,(expand-file-name
+                              (file-name-parent-directory data-directory))
+                            ,(expand-file-name
+                              (file-name-as-directory package-user-dir)))))))
+  (desktop-restore-eager 8)
+  (desktop-load-locked-desktop (if (version< "29" emacs-version) 'check-pid 'ask))
+  :config
+  ;; Don't save some buffers in desktop
+  (dolist (mode'(dired-mode
+                 Info-mode
+                 info-lookup-mode
+                 fundamental-mode
+                 helpful-mode
+                 helm-major-mode
+                 magit-mode
+                 magit-log-mode
+                 magit-status-mode
+                 magit-process-mode
+                 magit-diff-mode
+                 forge-pullreq-mode
+                 forge-notifications-mode
+                 difftastic-mode))
+    (add-to-list 'desktop-modes-not-to-save mode))
 
-(defun exordium--restore-desktop ()
-  "Restore desktop."
-  (setq desktop-path (list user-emacs-directory))
-  (setq desktop-save t)
-  (message (format "Loading desktop from %s" desktop-path))
-  (desktop-read)
-  (desktop-save-mode 1))
+  (add-to-list 'desktop-minor-mode-table '(company-posframe-mode nil))
 
-(if (not (daemonp))
-    (desktop-save-mode 1)
-  (add-hook 'server-after-make-frame-hook #'exordium--restore-desktop))
+  (if (daemonp)
+      (add-hook 'server-after-make-frame-hook #'exordium--restore-desktop)
+    (add-hook 'after-init-hook #'desktop-save-mode)))
 
 (savehist-mode t) ;; minibuffer history is saved
 
